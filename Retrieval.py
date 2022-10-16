90c5a7aba090a3dd789203d81840f22b82dca5c8
Oscar Aleksander Halvorsen <oscarah01@gmail.com>
2022-10-16 15:09:12 +0200

    make bag of words

--- Retrieval.py
+++ Retrieval.py
@@ -1,6 +1,7 @@
 import string
 import codecs
 import re
+from gensim import corpora
 from nltk.stem.porter import PorterStemmer
 
 def getText(txtFile):
@@ -18,10 +19,19 @@ def stemWords(dc):
 def filterOutStopwords(dc, stopWordFile):
     f = codecs.open(stopWordFile, "r", "utf-8")
     stopWords = re.split(',', f.readline())
-    return [[word for word in d if word not in stopWords] for d in dc]
-    
+    dc = [[word for word in d if word not in stopWords] for d in dc]
+    return [d for d in dc if d!=[]]
+
+def makeBagOfWords(dc):
+    dictionary = corpora.Dictionary()
+    return [dictionary.doc2bow(d,allow_update=True) for d in dc]
+
 txtFile = "pg3300.txt"
 stopWordFile ="stopwords.txt"
 dc = getDocumentCollection(txtFile)
 dc = filterOutStopwords(dc, stopWordFile)
-print(stemWords(dc))
+print(dc)
+print(len(dc))
+dc = makeBagOfWords(dc)
+print(dc)
+print(len(dc))

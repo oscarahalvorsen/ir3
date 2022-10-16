f7e9580f26ab6295756b33d03b2661c490f7e995
Oscar Aleksander Halvorsen <oscarah01@gmail.com>
2022-10-16 14:18:04 +0200

    revrote to one-liners, and added stemming

--- Retrieval.py
+++ Retrieval.py
@@ -1,7 +1,7 @@
-import random
 import string
 import codecs
 import re
+from nltk.stem.porter import PorterStemmer 
 
 def getText(txtFile):
     f = codecs.open(txtFile, "r", "utf-8")
@@ -9,15 +9,13 @@ def getText(txtFile):
 
 def getDocumentCollection(txtFile):
     dc = re.split('\r\n\s{0,}\r\n', getText(txtFile))
-    prossesedDc = []
-    for d in dc:
-        d = d.translate(str.maketrans('','',string.punctuation+"\n\r\t"))
-        if not bool(re.search('gutenberg', d.lower())):
-            prossesedDc.append(d.split())
-    return prossesedDc
+    return [d.translate(str.maketrans('','',string.punctuation+"\n\r\t")).split() for d in dc if (not bool(re.search('gutenberg', d.lower())))]
 
+def stemWords(dc):
+    stemmer = PorterStemmer()
+    return [[stemmer.stem(word.lower()) for word in d] for d in dc]
+    
 txtFile = "pg3300.txt"
 dc = getDocumentCollection(txtFile)
-print(dc)
+print(stemWords(dc))
 print(len(dc))
-
